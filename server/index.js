00c314fbf5ebf1436ab53e52044caf59696c80a4
tviewws <muthuikarume1972@gmail.com>
2026-06-07 13:12:15 +0300

    payments

--- server/index.js
+++ server/index.js
@@ -4,7 +4,9 @@ const cors = require('cors');
 
 const app = express();
 
-app.use(cors({ origin: 'http://localhost:3000' }));
+app.use(cors({
+  origin: ['http://localhost:3000', 'https://skinpeccable.vercel.app']
+}));
 app.use(express.json());
 
 // Routes
